c4ceb52df885129b6ae222a64aad56deb4fea706
Faisal El-Masri <unifaisal21@gmail.com>
2026-08-03 18:16:44 +1000

    Comparison Operators

---
+++ python-basics/lessons/comparison_operators.py
@@ -0,0 +1,21 @@
+#Example 1
+age = int(input("How old are you? "))
+print(age >= 18)
+
+#Example 2
+number = int(input("Enter a number: "))
+print(number > 100)
+
+#Example 3 
+password = input("Password: ")
+print(password == "python123")
+
+#Example 4
+username = input("Username: ")
+print(username != "admin")
+
+#Challenge
+ages = int(input("How old are you? "))
+print(ages >= 18)
+score = int(input("Give us a score? "))
+print(score > 50)
